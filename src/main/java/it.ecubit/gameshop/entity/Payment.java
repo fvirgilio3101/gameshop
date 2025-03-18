fb2c603c707be14409eeca5218fa97b701b4326c
Francesco <f.virgilio@ecubit.it>
2025-03-18 14:22:17 +0100

    create file properties

--- src/main/java/it.ecubit/gameshop/entity/Payment.java
+++ src/main/java/it.ecubit/gameshop/entity/Payment.java
@@ -25,7 +25,7 @@ public class Payment {
     public Long getIdPayment(){
         return this.idPayment;
     }
-    public Order getOrderId(){
+    public Order getOrder(){
         return this.order;
     }
     public boolean getPaymentStatus(){
