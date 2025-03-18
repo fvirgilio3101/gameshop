0e48a9fa76e92c64c1a4bce3975507168f904b59
Francesco <f.virgilio@ecubit.it>
2025-03-18 14:42:42 +0100

    created startup file and servlet file

---
+++ src/main/java/it.ecubit/gameshop/EcubitGameshopServiceApplication.java
@@ -0,0 +1,12 @@
+package it.ecubit.gameshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcubitGameshopServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcubitGameshopServiceApplication.class, args);
+    }
+}
