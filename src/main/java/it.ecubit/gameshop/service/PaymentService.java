3c9a3ec0ed94c9df9f411c8dd81bdd58d7c61dae
Francesco <f.virgilio@ecubit.it>
2025-03-20 17:37:44 +0100

    Created PaymentService and PaymentServiceImpl

--- src/main/java/it.ecubit/gameshop/service/PaymentService.java
+++ src/main/java/it.ecubit/gameshop/service/PaymentService.java
@@ -2,5 +2,14 @@ package it.ecubit.gameshop.service;
 
 import it.ecubit.gameshop.entity.Payment;
 
+import java.util.List;
+
 public interface PaymentService {
+
+    List<Payment> readAll();
+
+    Payment read(Payment payment);
+
+    Payment save(Payment payment);
+
 }
--- src/main/java/it.ecubit/gameshop/service/PaymentServiceImpl.java
+++ src/main/java/it.ecubit/gameshop/service/PaymentServiceImpl.java
@@ -1,7 +1,31 @@
 package it.ecubit.gameshop.service;
 
+import it.ecubit.gameshop.entity.Payment;
+import it.ecubit.gameshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class PaymentServiceImpl {
+public class PaymentServiceImpl implements PaymentService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+
+    @Override
+    public List<Payment> readAll() {
+        return this.paymentRepository.findAll();
+    }
+
+    @Override
+    public Payment read(Payment payment) {
+        return this.paymentRepository.getReferenceById(payment.getIdPayment());
+    }
+
+    @Override
+    public Payment save(Payment payment) {
+        return this.paymentRepository.save(payment);
+    }
 }
