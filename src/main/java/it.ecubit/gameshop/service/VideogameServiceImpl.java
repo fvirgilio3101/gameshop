2e8f95fe11d2f6ff3dbf8373926a2a89dc31e4ef
Ambrogio <a.franzese@ecubit.it>
2025-03-21 12:47:38 +0100

    added try catch and logs for service payment and videogame

--- src/main/java/it.ecubit/gameshop/service/VideogameServiceImpl.java
+++ src/main/java/it.ecubit/gameshop/service/VideogameServiceImpl.java
@@ -2,44 +2,98 @@ package it.ecubit.gameshop.service;
 
 import it.ecubit.gameshop.entity.Videogame;
 import it.ecubit.gameshop.repository.VideogameRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.List;
 
 @Service
-public class VideogameServiceImpl implements VideogameService{
+public class VideogameServiceImpl implements VideogameService {
+    private static final Logger log = LoggerFactory.getLogger(VideogameServiceImpl.class);
 
     @Autowired
     private VideogameRepository videogameRepository;
+
     @Override
     public List<Videogame> readAll() {
-        return this.videogameRepository.findAll();
+        log.info("Avvio lettura di tutti i videogiochi");
+        try {
+            List<Videogame> videogames = this.videogameRepository.findAll();
+            log.info("Lettura completata, trovati {} videogiochi", videogames.size());
+            return videogames;
+        } catch (Exception e) {
+            log.error("Errore durante la lettura dei videogiochi", e);
+            throw new RuntimeException("Errore durante il caricamento dei videogiochi");
+        }
     }
 
     @Override
     public Videogame read(Videogame videogame) {
-        return videogameRepository.getReferenceById(videogame.getIdVideogame());
+        log.info("Avvio lettura del videogioco con id {}", videogame.getIdVideogame());
+        try {
+            Videogame foundVideogame = this.videogameRepository.getReferenceById(videogame.getIdVideogame());
+            return foundVideogame;
+        } catch (Exception e) {
+            log.error("Errore durante la lettura dei videogiochi", e);
+            throw new EntityNotFoundException("Videogame con id" + videogame.getIdVideogame() + "non trovato");
+        }
     }
+
 
     @Override
     public List<Videogame> readByFilter(Videogame videogame) {
-        return this.videogameRepository.findAll(Example.of(videogame));
+        log.info("Avvio lettura dei videogiochi con filtro: {}", videogame);
+        try {
+            List<Videogame> filteredVideogames = this.videogameRepository.findAll(Example.of(videogame));
+            log.info("Lettura completata, trovati {} videogiochi con il filtro", filteredVideogames.size());
+            return filteredVideogames;
+        } catch (Exception e) {
+            log.error("Errore durante la lettura dei videogiochi con filtro", e);
+            throw new RuntimeException("Errore durante la lettura dei videogiochi con filtro");
+        }
     }
+
 
     @Override
     public Videogame save(Videogame videogame) {
-        return this.videogameRepository.save(videogame);
+        log.info("Avvio salvataggio del videogioco con id {}", videogame.getIdVideogame());
+        try {
+            Videogame savedVideogame = this.videogameRepository.save(videogame);
+            log.info("Videogame con id {} salvato correttamente", savedVideogame.getIdVideogame());
+            return savedVideogame;
+        } catch (Exception e) {
+            log.error("Errore durante il salvataggio del videogioco", e);
+            throw new RuntimeException("Errore durante il salvataggio del videogioco", e);
+        }
     }
+
 
     @Override
     public void deleteVideogame(Videogame videogame) {
-       this.videogameRepository.delete(videogame);
+        log.info("Avvio cancellazione del videogioco con id {}", videogame.getIdVideogame());
+        try {
+            this.videogameRepository.delete(videogame);
+            log.info("Videogame con id {} cancellato correttamente", videogame.getIdVideogame());
+        } catch (Exception e) {
+            log.error("Errore durante la cancellazione del videogioco con id {}", videogame.getIdVideogame(), e);
+            throw new RuntimeException("Errore durante la cancellazione del videogioco con id " + videogame.getIdVideogame(), e);
+        }
     }
+
 
     @Override
     public void deleteAll() {
-        this.videogameRepository.deleteAll();
+        log.info("Avvio cancellazione di tutti i videogiochi");
+        try {
+            this.videogameRepository.deleteAll();
+            log.info("Tutti i videogiochi sono stati cancellati correttamente");
+        } catch (Exception e) {
+            log.error("Errore durante la cancellazione di tutti i videogiochi", e);
+            throw new RuntimeException("Errore durante la cancellazione di tutti i videogiochi", e);
+        }
     }
 }
+
