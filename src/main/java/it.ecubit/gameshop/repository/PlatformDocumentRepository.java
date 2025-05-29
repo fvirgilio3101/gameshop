12af537f0e867d5f188835a3537160e7f84ade28
Ambrogio Franzese <a.franzese@ecubit.it>
2025-05-29 15:37:56 +0200

    changed platform from list to single string

--- src/main/java/it.ecubit/gameshop/repository/PlatformDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package it.ecubit.gameshop.repository;
-
-import it.ecubit.gameshop.document.PlatformDocument;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface PlatformDocumentRepository extends ElasticsearchRepository<PlatformDocument,Long> {
-}
